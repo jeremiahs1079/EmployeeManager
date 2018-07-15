ddf95a5fd5cbd791b090ee99f1ee00555e0b4ccd
Jeremiah Smith <jeremiahs1079@hotmail.com>
2018-07-15 08:28:36 -0600

    again

--- deploy.sh
+++ deploy.sh
@@ -126,6 +126,13 @@ if [ -e "$DEPLOYMENT_TARGET/.angular-cli.json" ]; then
   cd - > /dev/null
 fi
 
+# Post deployment stub
+if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
+  POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
+  cd "${POST_DEPLOYMENT_ACTION_DIR%\\*}"
+  "$POST_DEPLOYMENT_ACTION"
+  exitWithMessageOnError "post deployment action failed"
+fi
 
 ##################################################################################################################################
 echo "Finished successfully."
